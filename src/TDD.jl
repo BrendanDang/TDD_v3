f2ab0857f03840d00d50debbecf082403be0ecb7
Brendan Dang <b.dang@wustl.edu>
2023-03-17 18:11:49 -0500

    first attempt at graphConnect function

--- src/TDD.jl
+++ src/TDD.jl
@@ -2,4 +2,15 @@ module TDD
 
 # Write your package code here.
 
+#1-1: Given a graph and a starting node, return the list of connected nodes
+function graphConnect(map, node)
+    # Assume that node is in map and that map is given in ascending order
+    # Case where node is only connected to self
+    if node == map[node]
+        return [node]
+    end
+    # All other cases
+    return sort(vcat(node, map[node][1]))
+end
+
 end
