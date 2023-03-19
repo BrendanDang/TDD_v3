ce76d84566ea9663c8b7b5d77662941ca915aa34
Brendan Dang <b.dang@wustl.edu>
2023-03-19 10:34:17 -0500

    Added tests for 1-2 including directed graph

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,4 +11,20 @@ using Test
     @test TDD.graphConnect([[2,3],[1],[1],[5],[4]], [3]) == [1,3]
     @test TDD.graphConnect([[2,3],[1],[1],[5],[4]], [4]) == [4,5]
     @test TDD.graphConnect([[2,3],[1],[1],[5],[4]], [5]) == [4,5]
+
+    # 1-2 Given a graph and a starting node, return list of all nodes reachable
+    @test TDD.graphConnect([1], [1]) == [1]
+    @test TDD.graphConnect([[2,3],[1],[1],[5],[4]], [1]) == [1,2,3]
+    @test TDD.graphConnect([[2,3],[1],[1],[5],[4]], [2]) == [1,2,3]
+    @test TDD.graphConnect([[2,3],[1],[1],[5],[4]], [3]) == [1,2,3]
+    @test TDD.graphConnect([[2,3],[1],[1],[5],[4]], [4]) == [4,5]
+    @test TDD.graphConnect([[2,3],[1],[1],[5],[4]], [5]) == [4,5]
+    # Directed graph
+    @test TDD.graphConnect([[2,3],[3],[4,5],[6],[7], [1]]) == [1,2,3,4,5,6,7]
+    @test TDD.graphConnect([[2,3],[3],[4,5],[6],[7], [2]]) == [2,3,4,5,6,7]
+    @test TDD.graphConnect([[2,3],[3],[4,5],[6],[7], [3]]) == [3,4,5,6,7]
+    @test TDD.graphConnect([[2,3],[3],[4,5],[6],[7], [4]]) == [4,6]
+    @test TDD.graphConnect([[2,3],[3],[4,5],[6],[7], [5]]) == [5,7]
+    @test TDD.graphConnect([[2,3],[3],[4,5],[6],[7], [6]]) == [6]
+    @test TDD.graphConnect([[2,3],[3],[4,5],[6],[7], [7]]) == [7]
 end
