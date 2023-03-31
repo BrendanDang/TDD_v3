c2da235cad2a00e2e5ff5192f6761865c18967dd
Brendan Dang <b.dang@wustl.edu>
2023-03-31 17:42:57 -0500

    Think I fixed the logic and data types for boolean

--- src/graph.jl
+++ src/graph.jl
@@ -21,9 +21,41 @@ end
 
 function graphConnect(map, node, type::AbstractString)
     # 1-2: Given a graph and a starting node, return list of all nodes reachable
+    # 1-4: Generalize to take adjacency matrix maps
+        # map is now a Boolean array
+        # node is now an index
     # Indirect tracing
     bucketString = Int64[]
-    if (type == "indirect")
+    if (typeof(map) == Matrix{Bool})
+        for ele in node
+            bucketString = unique(sort(vcat(ele,findall(map[ele,1:end])))) # single node connectivity
+            nextBucketString = bucketString
+            secondBucketString = bucketString
+            #nextBucketString = findall(>(0), map[bucketString,1:end]) # the code we want to write, but returns vector of cartesian indexes
+            for subele in bucketString
+                nextBucketString = unique(sort(vcat(nextBucketString,findall(map[subele,1:end]))))
+            end
+            for subele2 in nextBucketString
+                secondBucketString = unique(sort(vcat(secondBucketString,findall(map[subele2,1:end]))))
+            end
+            if secondBucketString == bucketString
+                println("recursion")
+                return unique(sort(vcat(bucketString,nextBucketString)))
+            #if (findall(>(0),map[findall(>(0),map[ele,1:end])]) == findall(>(0),map[ele,1:end])) # recursion
+            #    bucketString = vcat(bucketString, findall(>(0), map[ele,1:end]), findall(>(0),map[findall(>(0), map[ele,1:end])]))
+            #    println("recursion?")
+            #    println(bucketString)
+            #    bucketString = unique(sort(bucketString))
+            else
+                neweles = bucketString[bucketString .∉ Ref(ele)]
+                bucketString = vcat(bucketString, nextBucketString, graphConnect(map, neweles,"indirect"))
+                println("not recursion?")
+                println(bucketString)
+                bucketString = unique(sort(bucketString))
+            end
+        end
+        return bucketString 
+    elseif (type == "indirect")
         for ele in node
             #println(ele)
             if ele > length(map)        # Terminal node
@@ -55,7 +87,11 @@ function graphConnect(map, type::AbstractString)
     d = Dict{Int64, Vector}()
     oa = [Int64[]]
     # Construct dictionary
-    if (type == "all")
+    if (typeof(map) == Matrix{Bool} && type == "all")
+        for a in 1:length(map[1,1:end])
+            d[a] = graphConnect(map,a,"indirect")
+        end
+    elseif (type == "all")
         for i in 1:length(map)
             d[i] = graphConnect(map, i, "indirect")
         end
@@ -63,14 +99,27 @@ function graphConnect(map, type::AbstractString)
     # Initialize output array 
     oa[1] = d[1]
     # Read through dictionary
-    for j in 1:length(map)
-        if d[j] ∉ oa
-            push!(oa,d[j])
+    if (typeof(map) == Matrix{Bool})
+        for b in 1:length(map[1,1:end])
+            if d[b] ∉ oa
+                push!(oa,d[b])
+            end
+        end
+    else
+        for j in 1:length(map)
+            if d[j] ∉ oa
+                push!(oa,d[j])
+            end
         end
     end
     return oa
 end
 
-#graphConnect([1], "all")
-#graphConnect([[2,3],[1],[1],[5],[4]], "all")
-#graphConnect([[2,3],[3],[4,5],[6],[7]], "all")
+# 1-4 for adjacency matrix format
+
+graphConnect([1], "all")
+graphConnect([[2,3],[1],[1],[5],[4]], "all")
+graphConnect([[2,3],[3],[4,5],[6],[7]], "all")
+graphConnect(Bool[1], "all")
+graphConnect(Bool[1 1 1 0 0; 1 1 0 0 0; 1 0 1 0 0; 0 0 0 1 1; 0 0 0 1 1], "all")
+graphConnect(Bool[1 1 1 0 0 0 0; 0 1 1 0 0 0 0; 0 0 1 1 1 0 0; 0 0 0 1 0 1 0; 0 0 0 0 1 0 1; 0 0 0 0 0 1 0; 0 0 0 0 0 0 1], "all")
