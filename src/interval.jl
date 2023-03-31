a4bdd92c1f9b166b8afe6464192e53c5bdcdd4d0
Brendan Dang <b.dang@wustl.edu>
2023-03-31 18:05:46 -0500

    first pass at creating structure and min/max

--- src/interval.jl
+++ src/interval.jl
@@ -1,1 +1,17 @@
 # Code for Problem 2 on Intervals
+
+# Define Interval Structure -- we're thinking we'll pass two floats to an interval structure
+struct Interval <: Integer
+    x
+    y
+end
+
+# Extend minimum
+function Base.:(minimum)(a::Interval)
+    return a.x
+end
+
+# Extend maximum
+function Base.:(maximum)(a::Interval)
+    return a.y
+end
