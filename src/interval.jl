a4174db51406e6018ff1bca49027fd75e4d6f18d
Brendan Dang <b.dang@wustl.edu>
2023-03-31 18:17:11 -0500

    changed tests, wrote code for 2-1 struct/min/max

--- src/interval.jl
+++ src/interval.jl
@@ -1,17 +1,20 @@
 # Code for Problem 2 on Intervals
 
 # Define Interval Structure -- we're thinking we'll pass two floats to an interval structure
-struct Interval <: Integer
-    x
-    y
+struct MyInterval
+    x::Float64
+    y::Float64
 end
 
 # Extend minimum
-function Base.:(minimum)(a::Interval)
+function Base.:(minimum)(a::MyInterval)
     return a.x
 end
 
 # Extend maximum
-function Base.:(maximum)(a::Interval)
+function Base.:(maximum)(a::MyInterval)
     return a.y
 end
+
+#@test TDD.minimum(MyInterval(2,9)) == 2 
+#MyInterval(2,9)
--- test/interval.jl
+++ test/interval.jl
@@ -1,5 +1,5 @@
 # Test for Problem 2 on Intervals
 
 #2-1
-@test TDD.minimum(Interval((2,9))) == 2 
-@test TDD.maximum(Interval((2,9))) == 9
+@test TDD.minimum(Interval(2,9)) == 2 
+@test TDD.maximum(Interval(2,9)) == 9
