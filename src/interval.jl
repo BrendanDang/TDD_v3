d3e7a006477abcb92dc4ffa34d1c97c87c93e416
Brendan Dang <b.dang@wustl.edu>
2023-03-31 21:29:01 -0500

    updated tests; wrote code for subsets

--- src/interval.jl
+++ src/interval.jl
@@ -25,6 +25,10 @@ function Base.:(in)(a::Int64,b::MyInterval)
     return a >= minimum(b) && a <= maximum(b)
 end
 
+function Base.:(in)(a::MyInterval,b::MyInterval)
+    return minimum(a) >= minimum(b) && maximum(a) <= maximum(b)
+end
+
 # Empty Interval
 function MyInterval(x::Real = NaN, y::Real = NaN)
     return MyInterval(x,y)
@@ -38,6 +42,26 @@ function Base.:(isempty)(a::MyInterval)
     end
 end
 
+# Subsets
+function Base.:(issubset)(a::MyInterval, b::MyInterval)
+    return a in b
+end
+
+function Base.:(issubset)(a::Vector{Float64}, b::MyInterval)
+    return a[1] in b && a[2] in b
+end
+
+function Base.:(issubset)(a::MyInterval, b::Vector{Float64})
+    return minimum(a) >= b[1] && maximum(a) <= b[2]
+end
+
+#function Base.:(iterate)(b::MyInterval)
+#    return iterate(minimum(b):maximum(b))
+#end
+
+#function Base.:(iterate)(b::MyInterval, c::Int64)
+#    return iterate(minimum(b):c:maximum(b))
+#end
 #minimum(MyInterval(2,9)) == 2 
 #maximum(MyInterval(2,9)) == 9
 
@@ -45,3 +69,6 @@ end
 #in(9.1,MyInterval(2,9)) == false
 
 #isempty(MyInterval()) == true
+#2.4
+#issubset(MyInterval(3.0,6.0),MyInterval(2.0,9.0))
+#issubset([1,5.3],MyInterval(2,9))
--- test/interval.jl
+++ test/interval.jl
@@ -12,5 +12,6 @@
 @test TDD.isempty(MyInterval()) == true
 
 #2.4
-@test TDD.issubset((2:5.3),MyInterval(2,9)) == true
-@test TDD.issubset((1:5.3),MyInterval(2,9)) == false
+@test TDD.issubset(MyInterval(3.0,6.0),MyInterval(2.0,9.0)) == true
+@test TDD.issubset([1,5.3],MyInterval(2,9)) == false
+@test TDD.issubset(MyInterval(3.5,4.5),[0.5,7.8]) == true
